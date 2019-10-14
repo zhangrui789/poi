b3b4bb4d4b1c69ab72d0a2293a77118543a07714
zr <1421308200@qq.com>
2019-10-14 13:25:11 +0800

    v1.1 add sout

--- src/main/java/com/zr/PoiApplication.java
+++ src/main/java/com/zr/PoiApplication.java
@@ -8,6 +8,7 @@ public class PoiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PoiApplication.class, args);
+        System.out.println("v1.o");
     }
 
 }
